45bc6d15ab01714693434b0731180e1cc9ed30bd
myhzw <215400737@qq.com>
2018-02-24 14:08:08 +0800

    引入iview的UI组件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import iView from 'iview'; // 导入组件库
+import 'iview/dist/styles/iview.css'; // 导入样式
 
 Vue.config.productionTip = false;
-
+Vue.use(iView);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
